810ec75cccb72777478d8cfa4402289f49437866
Georges Haidar <ghaidar0@gmail.com>
2023-12-01 20:55:50 +0000

    use array of dates

--- deepequals.test.ts
+++ deepequals.test.ts
@@ -1,39 +1,26 @@
 import { test, expect } from "bun:test";
 import * as z from "zod";
 
-const schema = z.array(
-  z.object({
-    dateTime: z
-      .string()
-      .datetime()
-      .transform((v) => new Date(v)),
-  })
-);
+const schema = z
+  .string()
+  .datetime()
+  .transform((v) => new Date(v))
+  .array();
 
 const createFixture = () => {
-  return {
-    dateTime: new Date("2020-01-01T00:00:00.001Z"),
-  } as const;
+  return new Date("2020-01-01T00:00:00.001Z");
 };
 
-test("Arrays of objects containing dates", async () => {
+test("Array of dates", async () => {
   const obj = createFixture();
 
   const parsed = schema.parse(JSON.parse(JSON.stringify([obj, obj])));
-
-  // These assertions work
-  expect(parsed[0].dateTime).toEqual(obj.dateTime);
-  expect(parsed[1].dateTime).toEqual(obj.dateTime);
-
-  // These also work
-  expect(parsed[0]).toEqual(obj);
-  expect(parsed[1]).toEqual(obj);
 
   console.log("Deep equals:", Bun.deepEquals(parsed, [obj, obj]));
   expect(parsed).toEqual([obj, obj]);
 });
 
-test("Arrays of objects containing primtive values", () => {
+test("Array of objects containing primtive values", () => {
   const gen = () => ({ foo: "bar" });
 
   expect([gen(), gen()]).toEqual([gen(), gen()]);
